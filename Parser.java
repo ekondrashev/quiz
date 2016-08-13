d5a2bd9f37799287641b8060108f6c678cfc76d9
snrdrum <shneor960@gmail.com>
2016-08-13 21:46:17 +0300

    Try with resources.

--- Parser.java
+++ Parser.java
@@ -1,3 +1,5 @@
+import sun.plugin.viewer.IExplorerPluginObject;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -13,30 +15,39 @@ public class Parser {
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+  public String getContent() {
     String output = "";
     int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
+    try (FileInputStream i = new FileInputStream(file)) {
+      while ((data = i.read()) > 0) {
         output += (char) data;
       }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
     return output;
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+  public String getContentWithoutUnicode() {
+    String output = "";
+    int data;
+    try (FileInputStream i = new FileInputStream(file)) {
+      while ((data = i.read()) > 0) {
+        if (data < 0x80) {
+          output += (char) data;
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return output;
+  }
+  public void saveContent(String content) {
+    try (FileOutputStream o = new FileOutputStream(file)) {
+      for (int i = 0; i < content.length(); i += 1) {
+        o.write(content.charAt(i));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
