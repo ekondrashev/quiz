902e24d588b1d60f1ac64502eedfe0a82eef9410
Антон <antonlubushko@gmail.com>
2016-09-22 14:05:07 +0300

    lock   in try-with-resources to autoclose

--- src/FileDocument.java
+++ src/FileDocument.java
@@ -18,40 +18,18 @@ public final class FileDocument implements Document {
     @Override
     public CharSequence read() throws IOException {
         byte[] allBytes;
-        FileChannel channel = null;
-        FileLock lock = null;
-        try {
-            channel = new RandomAccessFile(file, "r").getChannel();
-            lock = channel.tryLock(0, file.length(), true);
+        try (FileChannel channel = new RandomAccessFile(file, "r").getChannel();
+             FileLock lock = channel.tryLock(0, file.length(), true)) {
             allBytes = Files.readAllBytes(file.toPath());
-        } finally {
-            if (lock != null && lock.isValid()) {
-                lock.release();
-                lock.close();
-            }
-            if (channel != null && channel.isOpen()) {
-                channel.close();
-            }
         }
         return new String(allBytes);
     }
 
     @Override
     public void write(CharSequence content) throws IOException {
-        FileChannel channel = null;
-        FileLock lock = null;
-        try {
-            channel = new RandomAccessFile(file, "rw").getChannel();
-            lock = channel.tryLock(0, 0, true);
+        try (FileChannel channel = new RandomAccessFile(file, "rw").getChannel();
+             FileLock lock = channel.tryLock(0, 0, true)) {
             Files.write(file.toPath(), content.toString().getBytes());
-        } finally {
-            if (lock != null && lock.isValid()) {
-                lock.release();
-                lock.close();
-            }
-            if (channel != null && channel.isOpen()) {
-                channel.close();
-            }
         }
     }
 }
