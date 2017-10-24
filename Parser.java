5b9f12f5d2e553058456f45316475110a137e130
GreySlon <garanenkosergii@gmail.com>
2017-10-24 18:04:50 +0300

    refactored using decorator

--- Parser.java
+++ Parser.java
@@ -1,48 +1,15 @@
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
-/**
- * This class is thread safe.
- */
-public class Parser {
+public interface Parser {
 
-  private Reader reader;
-  private File file;
+  void setFile(File f);
 
-  public Parser(Reader reader) {
-    this.reader = reader;
-  }
+  File getFile();
 
-  public synchronized void setFile(File f) {
-    file = f;
-  }
+  String getContent() throws IOException;
 
-  public synchronized File getFile() {
-    return file;
-  }
+  String getContentWithoutUnicode() throws IOException;
 
-  public synchronized String getContent() throws IOException {
-    String output = "";
-    try (FileInputStream inputStream = new FileInputStream(file)) {
-      output = reader.read(inputStream);
-    }
-    return output;
-  }
-
-  public synchronized String getContentWithoutUnicode() throws IOException {
-    String output = "";
-    try (FileInputStream inputStream = new FileInputStream(file)) {
-      output = reader.read(inputStream, data -> data < 0x80);
-    }
-    return output;
-  }
-
-  public synchronized void saveContent(String content) throws IOException {
-    try (FileOutputStream outputStream = new FileOutputStream(file)) {
-      byte[] bytes = content.getBytes();
-      outputStream.write(bytes, 0, bytes.length);
-    }
-  }
+  void saveContent(String content) throws IOException;
 }
